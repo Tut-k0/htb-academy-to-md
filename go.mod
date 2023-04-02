ca7bb2e5b6f60d3959ca13c47f898d0d004670b8
Tut-k0 <Tut_k0@protonmail.com>
2023-04-02 17:26:48 -0500

    Add markdown package and test (WIP)

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module htb-academy-md
 
 go 1.20
 
-require golang.org/x/net v0.8.0
+require (
+	github.com/JohannesKaufmann/html-to-markdown v1.3.7
+	golang.org/x/net v0.8.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"os"
+)
 
 func main() {
-	options := getArguments()
-	session := authenticate(options.email, options.password)
-	title, content := getModule(options.moduleUrl, session)
-	fmt.Println(title, content)
+	// options := getArguments()
+	// session := authenticate(options.email, options.password)
+	// title, content := getModule(options.moduleUrl, session)
+	textBytes, err := os.ReadFile("test-1-page.html")
+	if err != nil {
+		die(err)
+	}
+	html := string(textBytes)
+	converter := md.NewConverter("", true, nil)
+	markdown, err := converter.ConvertString(html)
+	if err != nil {
+		die(err)
+	}
+
+	fmt.Println(markdown)
 }
